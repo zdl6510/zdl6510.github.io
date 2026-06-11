4644ff1370a304a7cd3e07fb79b901183c15b274
zdl6510 <jtsws34@qq.com>
2026-06-11 22:15:59 +0800

    Site updated: 2026-06-11 22:15:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["pages/Claude-Code是深入浅出-安装篇/","pages/Claude-Code深入浅出(技巧篇1)/"];function toRandomPost(){
+var posts=["pages/Claude-Code深入浅出(技巧篇1)/","pages/Claude-Code是深入浅出-安装篇/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
