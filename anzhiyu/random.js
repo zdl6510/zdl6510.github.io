a39b826f61d79e17887abf624b90c59fa415aebf
zdl6510 <jtsws34@qq.com>
2026-06-11 12:14:15 +0800

    Site updated: 2026-06-11 12:14:14

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/06/11/Claude-Code使用技巧篇/","2026/06/11/hello-world/"];function toRandomPost(){
+var posts=["2026/06/11/hello-world/","2026/06/11/Claude-Code使用技巧篇/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
